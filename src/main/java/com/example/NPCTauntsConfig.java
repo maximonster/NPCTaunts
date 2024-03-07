13bd6b6ad95c8b11c355d1aa648063a734bb77d6
max <maxreumer@gmail.com>
2024-03-07 16:41:45 +0100

    build plugin

---
+++ src/main/java/com/example/NPCTauntsConfig.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("NPCTaunts")
+public interface NPCTauntsConfig extends Config
+{
+	@ConfigItem(
+			keyName = "ForgetTime",
+			name = "Forget time",
+			description = "How long should everyone remember your death. Time specified in minutes",
+			position = 1
+	)
+	default int ForgetTimeDelay()
+	{
+		return 30;
+	}
+}
