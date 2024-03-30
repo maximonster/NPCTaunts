5a58e4e7765c8b59d894512298ac4db1689fff61
max <maxreumer@gmail.com>
2024-03-30 23:46:42 +0100

    Added colosseum

--- src/main/java/com/NPCTaunts/NPCTauntsPlugin.java
+++ src/main/java/com/NPCTaunts/NPCTauntsPlugin.java
@@ -17,7 +17,7 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-
+import net.runelite.client.util.Text;
 
 
 @Slf4j
@@ -47,7 +47,9 @@ public class NPCTauntsPlugin extends Plugin
 	static final int FIGHT_CAVE_REGION = 9551;
 	private boolean Jad;
 	static final int INFERNO_REGION = 9043;
+	static final int COLOSSEUM_REGION = 7216;
 	private boolean Zuk;
+	private boolean Sol;
 	private static final List<Integer> ZulRegionIDs = Arrays.asList(ZULRAH_SPAWN_REGION_ID,ZULRAH_REGION_ID);
 	private static final List<String> Gauntletminions = Arrays.asList("Crystalline Bat","Crystalline Rat","Crystalline Spider","Corrupted Rat","Corrupted Spider","Corrupted Bat");
 
@@ -55,6 +57,8 @@ public class NPCTauntsPlugin extends Plugin
 	private boolean jaddeath;
 	private boolean prezukdeath;
 	private boolean zukdeath;
+	private boolean presoldeath;
+	private boolean soldeath;
 	private boolean zulrahdeath;
 	private boolean prehunleffminion;
 	private boolean prehunleffdeath;
@@ -74,7 +78,7 @@ public class NPCTauntsPlugin extends Plugin
 	private final List<String> TowncrierPhrases = Arrays.asList("Hear ye, hear ye {Playername} has died to a {Enemyname}!","Listen all a {Enemyname} got the better of {Playername}");
 	private final List<String> PreJadPhrases = Arrays.asList("JalYts! JalYt {Playername} failed the fight caves!","JalYt {Playername} died fighting {Enemyname}!");
 	private final List<String> JadPhrases = Arrays.asList("JalYt {Playername} thought they could take a hit from TzTok-Jad","JalYt {Playername} thought they could outdamage Yt-HurKot","Can someone clean up JalYt {Playername}'s remains? TzTok-Jad made a mess again");
-	private final List<String> PreZukPhrases = Arrays.asList("JalYt {Playername} should avoid taking hits from Jal-Xil's heavy boulders","JalYt {Playername} should avoid taking hits from Jal-Zek's powerful magical abilities","JalYt {Playername} should avoid chasing Jal-Nib","JalYt {Playername} has succumbed to the inferno");
+	private final List<String> PreZukPhrases = Arrays.asList("JalYt {Playername} should avoid taking hits from Jal-Xil's heavy boulders","JalYt {Playername} should avoid taking hits from Jal-Zek's powerful magical abilities","JalYt {Playername} should avoid chasing Jal-Nib","JalYt {Playername} has succumbed to the inferno","Cmon man its easier than the colloseum");
 	private final List<String> ZukPhrases = Arrays.asList("TzKal-Zuk is unbeatable","Was it worth your fire cape?","JalYt {Playername} you must stay behind the shield");
 	//If you die to a rat spider or bat
 	private final List<String> BrynrsbPhrases = Arrays.asList("You died to a {Enemyname}????","How in Seren's name did you die to a {Enemyname}?");
@@ -84,15 +88,21 @@ public class NPCTauntsPlugin extends Plugin
 	private final List<String> BrynPhrases = Arrays.asList("Hey everyone {Playername} died so will get some cod!","Get stomped","Trying running away from the tornadoes","The floor really is lava","Standing still will get you nill","This one would've been the Enhanced weapon seed");
 	private final List<String> ZulPhrases = Arrays.asList("Snake?? SNAKE??? SNAAAAAAAAAAAAAAAAAAKEEE","You have blessed us with a bountiful eel harvest","Zulrah was pleased","Your sacrifice is appreciated","The sacred contract has been fullfilled once again","Zulrah dislikes the taste of your equipment");
 	private final List<String> OlmPhrases = Arrays.asList("No purple for you","If you scuff I will not provide stuff");
+	private final List<String> PreSolPhrases = Arrays.asList("A {Title} has passed away!","{Playername} made it to wave {Wave}","{Mod} was too much for you","Death smiles at us all. All a man can do is smile back","Were you not entertained?","This is FORTIS","Maybe don't select {Mod} next time","Cmon man its easier than inferno","Morituri te salutant");
+	private final List<String> SolPhrases = Arrays.asList("A {Title} was defeated by Sol Heredit!","Sol is known for twisting your knob off","People should know when they are conquered","{Mod} makes Sol stronger");
 
 	private final List<String> SixNinePhrases = Arrays.asList("hehe nice");
 	private final List<String> SevenThreePhrases = Arrays.asList("You little ginger prick","Thanks for the zerker ring man");
 	private final List<String> OnePhrases = Arrays.asList("Seriously, you died to a 1? Eat a cabbage next time, then you might survive. Yuck.","Gratz on your Nightmare Mode loss","EAT {Playername}! EAT!");
 
 	private List<Integer> Recentlycommented = new ArrayList<>();
-
-
-
+	private String colotitle = "";
+private int wave;
+	private static final int VARBIT_MODIFIER_SELECTED = 9788;
+	private static final int SCRIPT_MODIFIER_SELECT_INIT = 4931;
+	private final List<Integer> modifierOptions = new ArrayList<>(3);
+private int Modifierselected =-1;
+	private final List<String> modifiers = Arrays.asList("The Doom Scorpion","Reentry","Bees!","Volatility","Blasphemy","Relentless","Quartet","Totemic","Doom","Dynamic Duo","Solarflare","Myopia","Frailty","Red Flag");
 
     @Override
 	protected void startUp() throws Exception
@@ -108,11 +118,14 @@ public class NPCTauntsPlugin extends Plugin
 		jaddeath=false;
 		prezukdeath=false;
 		zukdeath=false;
+		presoldeath=false;
+		soldeath=false;
 		zulrahdeath=false;
 		prehunleffminion=false;
 		prehunleffdeath=false;
 		hunleffdeath=false;
 		chambersdeath=false;
+		Modifierselected =-1;
 		Recentlycommented.clear();
 		log.info("NPC Taunts stopped!");
 
@@ -145,6 +158,26 @@ public class NPCTauntsPlugin extends Plugin
 			String playername = (String) client.getLocalPlayer().getName();
 			s = s.replace("{Playername}", playername);
 		}
+		if (s.contains("{Title}"))
+		{
+			s = s.replace("{Title}", colotitle);
+		}
+		if (s.contains("{Wave}"))
+		{
+			String Wave = Integer.toString(wave);
+			s = s.replace("{Wave}", Wave);
+		}
+		if (s.contains("{Mod}"))
+		{
+			if (Modifierselected ==-1)
+			{
+				s = s.replace("{Mod}", "That last modifier");;
+			}
+			else{
+			String mod = modifiers.get(Modifierselected);
+			s = s.replace("{Mod}", mod);
+			}
+		}
 		return s;
 	}
 
@@ -163,6 +196,61 @@ public class NPCTauntsPlugin extends Plugin
 			log.debug("Most recent death has been forgotten");
 		}
 	}
+	@Subscribe
+	public void onChatMessage(ChatMessage event)
+	{
+		String chatMsg = Text.removeTags(event.getMessage()); //remove color and linebreaks
+		log.debug(chatMsg);
+		if (chatMsg.startsWith("Minimus: A") && (chatMsg.contains("approaches!")))
+		{
+			String colosseumtitle = chatMsg.replace("Minimus: A ","").replace(" approaches!","");
+			colotitle = colosseumtitle;
+		}
+		if (chatMsg.startsWith("Wave:"))
+		{
+			wave =  Integer.valueOf(chatMsg.replace("Wave: ",""));
+		}
+	}
+
+	@Subscribe
+	public void onScriptPreFired(ScriptPreFired pre)
+	{
+		if (pre.getScriptId() == SCRIPT_MODIFIER_SELECT_INIT)
+		{
+			modifierOptions.clear();
+			Object[] args = pre.getScriptEvent().getArguments();
+			modifierOptions.add((Integer) args[2]);
+			modifierOptions.add((Integer) args[3]);
+			modifierOptions.add((Integer) args[4]);
+		}
+	}
+
+	private void SelectedModifier()
+	{
+		if (modifierOptions.isEmpty())
+		{
+			log.debug("Modifier options were not loaded");
+			return;
+		}
+
+		int selectedoption = client.getVarbitValue(VARBIT_MODIFIER_SELECTED);
+		if (selectedoption == 0)
+		{
+			log.debug("Modifier selected varbit = 0");
+			return;
+		}
+
+		Modifierselected = modifierOptions.get(selectedoption - 1);
+		modifierOptions.clear();
+		if (Modifierselected == -1)
+		{
+			log.debug("Failed to select modifier");;
+		}
+
+	}
+
+
+
 
 	@Subscribe
 	public void onInteractingChanged(InteractingChanged event)
@@ -224,6 +312,8 @@ public class NPCTauntsPlugin extends Plugin
 				jaddeath=false;
 				prezukdeath=false;
 				zukdeath=false;
+				presoldeath=false;
+				soldeath=false;
 				zulrahdeath=false;
 				prehunleffminion=false;
 				prehunleffdeath=false;
@@ -258,6 +348,17 @@ public class NPCTauntsPlugin extends Plugin
 					else
 					{
 						prezukdeath = true;
+					}
+				}
+				if (regionId == COLOSSEUM_REGION)
+				{
+					if (Sol)
+					{
+						soldeath = true;
+					}
+					else
+					{
+						presoldeath = true;
 					}
 				}
 
@@ -308,6 +409,10 @@ public class NPCTauntsPlugin extends Plugin
 		if (event.getNpc().getName().equals("TzKal-Zuk"))
 		{
 			Zuk = true;
+		}
+		if (event.getNpc().getName().equals("Sol Heredit"))
+		{
+			Sol = true;
 		}
 
 		if (event.getNpc().getName().equals("Great Olm"))
@@ -436,6 +541,23 @@ public class NPCTauntsPlugin extends Plugin
 			event.getNpc().setOverheadText(taunt);
 			Recentlycommented.add(event.getNpc().getId());
 		}
+		if (event.getNpc().getName().equals("Minimus") && getRegionId() != COLOSSEUM_REGION)
+		{
+			if (presoldeath)
+			{
+				taunt = Phrase(PreSolPhrases);
+			}
+			else if (soldeath)
+			{
+				taunt = Phrase(SolPhrases);
+			}
+			else{
+				return;
+			}
+			event.getNpc().setOverheadCycle(300);
+			event.getNpc().setOverheadText(taunt);
+			Recentlycommented.add(event.getNpc().getId());
+		}
 
 		if (event.getNpc().getName().equals("Bryn"))
 		{
@@ -513,6 +635,12 @@ public class NPCTauntsPlugin extends Plugin
 		if (event.getNpc().getName().equals("TzKal-Zuk"))
 		{
 			Zuk = false;
+		}
+		final int regionId = getRegionId();
+		if (regionId == COLOSSEUM_REGION && event.getNpc().getName().equals("Minimus"))
+		{
+			log.debug("Modifier selected");
+			SelectedModifier();
 		}
 	}
 
